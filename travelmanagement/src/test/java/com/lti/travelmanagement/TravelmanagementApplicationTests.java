399b8333563031e09ede0a642f6fc9f61c890e54
ankita-1812 <tripathiankita242@gmail.com>
2022-11-26 20:10:17 +0530

    project config

---
+++ travelmanagement/src/test/java/com/lti/travelmanagement/TravelmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lti.travelmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
