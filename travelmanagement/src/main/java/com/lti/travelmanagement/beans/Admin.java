3bd8f60363c124b5ca6b8b696825efdf8e8c2137
HarshitaSharma <sharshita165@gmail.com>
2022-11-27 11:49:21 +0530

    file update

--- travelmanagement/src/main/java/com/lti/travelmanagement/beans/Admin.java
+++ travelmanagement/src/main/java/com/lti/travelmanagement/beans/Admin.java
@@ -1,10 +1,10 @@
 package com.lti.travelmanagement.beans;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+//import javax.persistence.Entity;
+//import javax.persistence.Table;
 
-@Entity
-@Table(name="admin")
+//@Entity
+//@Table(name="admin")
 public class Admin {
 
 	
