18121a68ad39fd05742221bf28de1fc5387909ea
ankita-1812 <tripathiankita242@gmail.com>
2022-11-27 15:17:46 +0530

    travelexpense and travelrequest changes updated

--- travelmanagement/src/main/java/com/lti/travelmanagement/beans/TravelExpense.java
+++ travelmanagement/src/main/java/com/lti/travelmanagement/beans/TravelExpense.java
@@ -10,12 +10,52 @@ import javax.persistence.Table;
 @Table(name="travel_expense")
 public class TravelExpense {
 
+	
 	@Id
 	private int travel_expense_id;
 	@OneToOne(cascade = CascadeType.ALL)
 	private TravelRequest travelrequest;
 	private String travel_expense_date;
 	private String travel_expense_status;
+	public TravelExpense(int travel_expense_id, TravelRequest travelrequest, String travel_expense_date,
+			String travel_expense_status) {
+		super();
+		this.travel_expense_id = travel_expense_id;
+		this.travelrequest = travelrequest;
+		this.travel_expense_date = travel_expense_date;
+		this.travel_expense_status = travel_expense_status;
+	}
+	public int getTravel_expense_id() {
+		return travel_expense_id;
+	}
+	public void setTravel_expense_id(int travel_expense_id) {
+		this.travel_expense_id = travel_expense_id;
+	}
+	public TravelRequest getTravelrequest() {
+		return travelrequest;
+	}
+	public void setTravelrequest(TravelRequest travelrequest) {
+		this.travelrequest = travelrequest;
+	}
+	public String getTravel_expense_date() {
+		return travel_expense_date;
+	}
+	public void setTravel_expense_date(String travel_expense_date) {
+		this.travel_expense_date = travel_expense_date;
+	}
+	public String getTravel_expense_status() {
+		return travel_expense_status;
+	}
+	public void setTravel_expense_status(String travel_expense_status) {
+		this.travel_expense_status = travel_expense_status;
+	}
+	@Override
+	public String toString() {
+		return "TravelExpense [travel_expense_id=" + travel_expense_id + ", travelrequest=" + travelrequest
+				+ ", travel_expense_date=" + travel_expense_date + ", travel_expense_status=" + travel_expense_status
+				+ "]";
+	}
+	
 	
 	
 
--- travelmanagement/src/main/java/com/lti/travelmanagement/beans/TravelRequest.java
+++ travelmanagement/src/main/java/com/lti/travelmanagement/beans/TravelRequest.java
@@ -14,6 +14,65 @@ public class TravelRequest {
 	private String travel_request_s_date;
 	private String travel_request_e_date;
 	private String travel_request_reason;
+	public TravelRequest() {
+		
+	}
+	public TravelRequest(int travel_request_id, Employee employee, String travel_request_date,
+			String travel_request_status, String travel_request_s_date, String travel_request_e_date,
+			String travel_request_reason) {
+		super();
+		this.travel_request_id = travel_request_id;
+		this.employee = employee;
+		this.travel_request_date = travel_request_date;
+		this.travel_request_status = travel_request_status;
+		this.travel_request_s_date = travel_request_s_date;
+		this.travel_request_e_date = travel_request_e_date;
+		this.travel_request_reason = travel_request_reason;
+	}
+	public int getTravel_request_id() {
+		return travel_request_id;
+	}
+	public void setTravel_request_id(int travel_request_id) {
+		this.travel_request_id = travel_request_id;
+	}
+	public Employee getEmployee() {
+		return employee;
+	}
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	public String getTravel_request_date() {
+		return travel_request_date;
+	}
+	public void setTravel_request_date(String travel_request_date) {
+		this.travel_request_date = travel_request_date;
+	}
+	public String getTravel_request_status() {
+		return travel_request_status;
+	}
+	public void setTravel_request_status(String travel_request_status) {
+		this.travel_request_status = travel_request_status;
+	}
+	public String getTravel_request_s_date() {
+		return travel_request_s_date;
+	}
+	public void setTravel_request_s_date(String travel_request_s_date) {
+		this.travel_request_s_date = travel_request_s_date;
+	}
+	public String getTravel_request_e_date() {
+		return travel_request_e_date;
+	}
+	public void setTravel_request_e_date(String travel_request_e_date) {
+		this.travel_request_e_date = travel_request_e_date;
+	}
+	public String getTravel_request_reason() {
+		return travel_request_reason;
+	}
+	public void setTravel_request_reason(String travel_request_reason) {
+		this.travel_request_reason = travel_request_reason;
+	}
+	
+	
 	
 	
 	
